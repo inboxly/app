7560d46bb74fd9e2088414fa15924e3f7ed7304d
sentosango <sentosango@gmail.com>
2021-07-02 22:57:50 +0300

    Add entry list to main page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,15 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page>
+    <entry-list url="http://127.0.0.1:8000/api/entries?unreadOnly=1&api_token=api_token"/>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import EntryList from 'components/EntryList'
 
 export default defineComponent({
   name: 'PageIndex',
+  components: { EntryList },
 })
 </script>
