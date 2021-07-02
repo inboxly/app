13a290b6c9249601d5efa2d2111dc38f8c0d50bf
sentosango <sentosango@gmail.com>
2021-07-02 22:57:15 +0300

    Improve main layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,22 +2,32 @@
   <q-layout view="lHr lpR lFr">
 
     <q-header reveal bordered class="bg-black text-white">
-      <q-toolbar>
-        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer"/>
+      <q-toolbar class="row">
+        <div class="col text-left">
+          <q-circular-progress
+            show-value
+            class="text-white q-ml-xs"
+            :max="875"
+            :value="142"
+            size="1.6rem"
+            color="white"
+            track-color="grey"
+          />
+        </div>
 
-        <q-toolbar-title>
-          <q-avatar size="2rem">
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-          Inboxly
+        <q-toolbar-title class="text-center text-subtitle1 col-7">
+          All personal feeds
         </q-toolbar-title>
 
-        <q-btn dense flat round icon="menu" @click="toggleRightDrawer"/>
+        <div class="col text-right">
+          <q-btn dense flat round icon="done"/>
+          <q-btn dense flat round icon="more_horiz"/>
+        </div>
       </q-toolbar>
     </q-header>
 
     <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
-      <!-- drawer content -->
+     <main-menu/>
     </q-drawer>
 
     <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
@@ -28,10 +38,14 @@
       <router-view/>
     </q-page-container>
 
-    <q-footer bordered class="bg-black text-white">
-      <q-toolbar>
-
-      </q-toolbar>
+    <q-footer reveal bordered class="bg-black text-white">
+      <q-btn-group spread class="q-pa-xs">
+        <q-btn dense flat round :ripple="false" icon="subject" @click="toggleLeftDrawer"/>
+        <q-btn dense flat round :ripple="false" icon="bookmark_border"/>
+        <q-btn dense flat round :ripple="false" icon="grid_view" class="text-primary"/>
+        <q-btn dense flat round :ripple="false" icon="add"/>
+        <q-btn dense flat round :ripple="false" icon="search"/>
+      </q-btn-group>
     </q-footer>
 
   </q-layout>
@@ -39,8 +53,10 @@
 
 <script>
 import { ref } from 'vue'
+import MainMenu from 'components/MainMenu'
 
 export default {
+  components: { MainMenu },
   setup () {
     const leftDrawerOpen = ref(false)
     const rightDrawerOpen = ref(false)
