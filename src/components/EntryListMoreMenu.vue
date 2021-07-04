01e0f4a017c99a4745c5a0fb5f0daadedb5a8285
sentosango <sentosango@gmail.com>
2021-07-04 12:50:54 +0300

    Add EntryListMoreMenu

---
+++ src/components/EntryListMoreMenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-dialog
+    :value='value' @input="newValue => $emit('input', newValue)"
+    transition-show="slide-up" transition-hide="slide-down" maximized auto-close
+  >
+    <div class="bg-dark">
+      <top-bar title="Customize feed">
+        <template #right>
+          <q-btn dense>Done</q-btn>
+        </template>
+      </top-bar>
+    </div>
+  </q-dialog>
+</template>
+
+<script>
+import TopBar from 'components/TopBar'
+export default {
+  name: 'EntryListMoreMenu',
+  components: { TopBar },
+  props: ['value'],
+  setup () {
+    return {}
+  },
+}
+</script>
