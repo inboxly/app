73ed235b545f4e977b0d0d4ad0933df7154089b1
sentosango <sentosango@gmail.com>
2021-07-04 12:59:21 +0300

    Add more menu to index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,6 +2,7 @@
   <q-page>
     <top-bar title="All personal feeds" progress done more/>
     <entry-list url="http://127.0.0.1:8000/api/entries?unreadOnly=1&api_token=api_token"/>
+    <entry-list-more-menu v-model="moreMenu"/>
   </q-page>
 </template>
 
@@ -9,9 +10,16 @@
 import { defineComponent } from 'vue'
 import EntryList from 'components/EntryList'
 import TopBar from 'components/TopBar'
+import ProgressCounter from 'components/ProgressCounter'
+import EntryListMoreMenu from 'components/EntryListMoreMenu'
 
 export default defineComponent({
   name: 'PageIndex',
-  components: { TopBar, EntryList },
+  components: { EntryListMoreMenu, ProgressCounter, TopBar, EntryList },
+  data () {
+    return {
+      moreMenu: false,
+    }
+  },
 })
 </script>
