8fde2b32a4cf62b1545d590dcc4db1fea9b811f7
sentosango <sentosango@gmail.com>
2021-07-11 01:24:56 +0300

    Don't use bar component in EntryListMenuOverlay (it causes unexpected behavior of markup)

--- src/components/overlays/EntryListMenuOverlay.vue
+++ src/components/overlays/EntryListMenuOverlay.vue
@@ -3,21 +3,23 @@
     :value='value' @input="newValue => $emit('input', newValue)"
     transition-show="slide-up" transition-hide="slide-down" maximized auto-close
   >
-    <div class="bg-dark">
-      <bar title="Customize feed">
-        <template #right>
-          <q-btn dense>Done</q-btn>
-        </template>
-      </bar>
+    <div class="bg-dark ">
+        <q-toolbar class="row">
+          <div class="col text-left"></div>
+          <q-toolbar-title class="text-center text-subtitle1 col-7">
+            Customize feed
+          </q-toolbar-title>
+          <div class="col text-right">
+            <q-btn dense>Done</q-btn>
+          </div>
+        </q-toolbar>
     </div>
   </q-dialog>
 </template>
 
 <script>
-import Bar from 'components/layout/Bar'
 export default {
   name: 'EntryListMenuOverlay',
-  components: { Bar },
   props: ['value'],
   setup () {
     return {}
